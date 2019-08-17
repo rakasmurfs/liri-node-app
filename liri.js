32af1b7295bfc1a78596345afd822946af30dccf
rakasmurfs <rakasmurfs@gmail.com>
2019-08-16 21:57:49 -0400

    Added keys and liri.js

---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+//console.log(spotify);
+function concertThis()
+    {
+    };
+function spotifyThis()
+    {
+
+    };
+function movieThis()
+    {
+        
+    };
+function doThis()
+    {
+        
+    };
